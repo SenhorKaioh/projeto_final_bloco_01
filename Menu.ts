bf879dd48d424c3218a4fec0f6c531e46b07cc2a
Caio <caiopsantos.dev@gmail.com>
2024-04-03 11:37:27 -0300

    CriacaoDoMenu

---
+++ Menu.ts
@@ -0,0 +1,64 @@
+import * as leia from 'readline-sync';
+
+export function main(){
+
+let cod: number;
+
+ do{
+
+    console.log("|*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|")
+    console.log("|                                             -|")
+    console.log("| LOJAS CARIOCAS - Informática e  Periféricos -|")
+    console.log("|                                             -|")
+    console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--|");
+    console.log("|                                             -|");
+    console.log("|        1 - Listar todos os Produtos         -|");
+    console.log("|        2 - Listar Produto por id             |");
+    console.log("|        3 - Adicionar o Produto              -|");
+    console.log("|        4 - Atualizar Produto Cadastrado     -|");
+    console.log("|        5 - Deletar o produto                -|");
+    console.log("|        0 - Sair                             -|");
+    console.log("|                                             -|");
+    console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--|");
+
+        console.log("Entre com a opção desejada: ")
+        cod = leia.questionInt("");
+
+        switch(cod){
+            case 1: 
+              console.log("\nLista de todos os produtos: \n");
+                    
+              break;
+            
+            case 2:
+              console.log("\nConsulta do produto por ID: \n");
+               
+              break;
+  
+            case 3: 
+              console.log("\nAdiconar Novo Produto: \n");
+             
+              break;
+            case 4:
+                console.log("\nAtualizar dados do Produto: \n");
+                 
+                break;
+    
+            case 5: 
+                console.log("\nApagar o Produto: \n");
+               
+                break;  
+            case 0:
+              console.log("\nO Programa foi Finalizado!");
+            break;
+  
+            default:
+              console.log("\nOpção Inválida\n");  
+  
+         }
+
+ }while(cod !== 0);
+
+
+}
+main()
